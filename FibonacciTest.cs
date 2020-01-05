91abb64476a77979a23cc1a7fd4152fe1be1a330
stevekeyen@gmail.com <Labyrinth_13>
2020-01-05 14:31:06 +0100

    added Fibonacci test after seeing Tool @ Werchter

---
+++ FibonacciTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        private int CaclFibonacciNumber(int position)
+        {
+            if (position == 0) return 0;
+            if (position <= 2) return 1;
+            var calc = CaclFibonacciNumber(position - 1) + CaclFibonacciNumber(position - 2);
+            return calc;
+        }
+
+        [TestMethod]
+        public void TestFibonacci()
+        {
+            int[,] intArray = { 
+                                { 0, 0 }, 
+                                { 1, 1 }, 
+                                { 2, 1 },
+                                { 3, 2 },
+                                { 4, 3 },
+                                { 5, 5},
+                                { 6, 8 },
+                                { 7, 13 },
+                                { 8, 21 },
+                                { 9, 34 },
+                                { 10, 55 },
+                                { 11, 89 },
+                                { 12, 144 },
+                                { 13, 233},
+                                { 14, 377 },
+                                { 15, 610 },
+                              };
+
+            
+            for(int i = 0; i < intArray.GetLength(0); i++)
+            {
+                System.Diagnostics.Debug.WriteLine(i.ToString());
+                Assert.AreEqual(intArray[i,1], CaclFibonacciNumber(intArray[i,0]));
+            }
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fibonacci")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fibonacci")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("179fecdb-d41d-4e04-9ade-b77a33c574d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
